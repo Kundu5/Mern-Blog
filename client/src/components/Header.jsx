871f9b94ce3f4fb93093b99b0214cba8ad36fa77
Kundu5 <kundansirvi16@gmail.com>
2024-10-07 09:57:13 +0530

    create signup page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
   const path = useLocation().pathname;
   
   return (
-    <Navbar className='border-b-2'>
+    <Navbar className='border-b-2 bg-purple-100'>
       <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
         <span className='px-2 py-1 bg-gradient-to-r from-indigo-300 via-purple-400 to bg-pink-300 rounded-lg text-white'>CODEVERSE</span>
       </Link>
