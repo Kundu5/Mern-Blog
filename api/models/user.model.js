715d2319819d650fea37cbda952a440e048522b4
Kundu5 <kundansirvi16@gmail.com>
2024-10-20 15:41:29 +0530

    admin function to the user

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -24,7 +24,11 @@ const userSchema = new mongoose.Schema({
         default: 'https://imgs.search.brave.com/O6rLvO8-5IAjWakgOKrUpBMPRo2IDSw78luSsXhnQbs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/cG5naXRlbS5jb20v/cGltZ3MvbS8zMC0z/MDc0MTZfcHJvZmls/ZS1pY29uLXBuZy1p/bWFnZS1mcmVlLWRv/d25sb2FkLXNlYXJj/aHBuZy1lbXBsb3ll/ZS5wbmc',
 
     },
-
+    
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
 
     }, {timestamps: true}
 );
