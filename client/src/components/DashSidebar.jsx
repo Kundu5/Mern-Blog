88bb71ab030dccdff4562616bbcf409deddf76a0
Kundu5 <kundansirvi16@gmail.com>
2024-10-20 19:57:46 +0530

    users info on the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Sidebar} from 'flowbite-react';
-import {HiUser, HiArrowSmRight, HiDocumentText} from 'react-icons/hi';
+import {HiUser, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup} from 'react-icons/hi';
 import {Link, useLocation} from 'react-router-dom';
 import { signoutSuccess } from '../redux/user/userSlice';
 import { useDispatch, useSelector } from 'react-redux';
@@ -52,7 +52,13 @@ export default function DashSidebar() {
                 </Sidebar.Item>
                 </Link>
                 )}
-                
+                {currentUser.isAdmin && (
+                  <Link to='/dashboard?tab=users'>
+                    <Sidebar.Item active={tab === 'users'} icon={HiOutlineUserGroup} as='div'>
+                      Users
+                    </Sidebar.Item>
+                  </Link>
+                )}
                 <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignout}>
                         Sign Out
                     </Sidebar.Item>
